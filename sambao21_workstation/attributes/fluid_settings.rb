3bbfb41cad9e654f0e0cdb18a09003610a8a960a
Matt "Trip" Maker <trip@monstro.us>
2013-10-30 11:04:30 -0500

    add config for creating Fluid apps

---
+++ sambao21_workstation/attributes/fluid_settings.rb
@@ -0,0 +1,23 @@
+default['fluid']['datadir'] = "#{node['sprout']['home']}/Library/Application Support/Fluid/FluidApps"
+
+default['sprout']['fluid']['instances']['google-calendar']['apptitle'] = "Fluid Google Calendar"
+default['sprout']['fluid']['instances']['google-calendar']['basename'] = "fluid-google-calendar"
+
+default['sprout']['fluid']['instances']['facebook']['apptitle'] = "Fluid Facebook"
+default['sprout']['fluid']['instances']['facebook']['basename'] = "fluid-facebook"
+
+default['sprout']['fluid']['instances']['blinksale']['apptitle'] = "Fluid Blinksale"
+default['sprout']['fluid']['instances']['blinksale']['basename'] = "fluid-blinksale"
+
+default['sprout']['fluid']['instances']['google-apps-monstrous']['apptitle'] = "Fluid Google Apps Monstrous"
+default['sprout']['fluid']['instances']['google-apps-monstrous']['basename'] = "fluid-google-apps-monstrous"
+
+default['sprout']['fluid']['instances']['google-apps-sasona']['apptitle'] = "Fluid Google Apps Sasona"
+default['sprout']['fluid']['instances']['google-apps-sasona']['basename'] = "fluid-google-apps-sasona"
+
+default['sprout']['fluid']['instances']['twitter-mattmaker']['apptitle'] = "Fluid Twitter Mattmaker"
+default['sprout']['fluid']['instances']['twitter-mattmaker']['basename'] = "fluid-twitter-mattmaker"
+
+default['sprout']['fluid']['instances']['twitter-triple_entendre']['apptitle'] = "Fluid Twitter triple_entendre"
+default['sprout']['fluid']['instances']['twitter-triple_entendre']['basename'] = "fluid-twitter-triple_entendre"
+
