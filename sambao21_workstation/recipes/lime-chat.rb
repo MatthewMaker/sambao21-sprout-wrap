056e75abe953f6ce69da9e407f4d7c159e695954
Matt "Trip" Maker <trip@monstro.us>
2013-10-30 10:47:39 -0500

    make limechat auto-start at login

--- sambao21_workstation/recipes/lime-chat.rb
+++ sambao21_workstation/recipes/lime-chat.rb
@@ -1,2 +1,9 @@
 include_recipe "homebrew::cask"
 homebrew_cask "lime-chat"
+
+# start up on login
+execute "Start LimeChat automatically" do
+    command "addLoginItem \"/Applications/LimeChat.app\""
+    user node['current_user']
+  returns [0, 126]  # 126 means it's already in there!
+end
